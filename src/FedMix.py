156a4c21a19670e575180db4306c4a940311a5f7
Elnur Gasanov <gasanoe@kw61115.kaust.edu.sa>
2021-10-30 20:36:59 +0300

    FedMix debugging 1

--- src/FedMix.py
+++ src/FedMix.py
@@ -1,4 +1,4 @@
-from typing import Any, Callable, Mapping, Sequence, Tuple
+from typing import Any, Callable, Mapping, Sequence, Tuple, Iterable, Dict
 
 from fedjax.core import client_datasets
 from fedjax.core import dataclasses
@@ -7,22 +7,26 @@ from fedjax.core import federated_data
 from fedjax.core import for_each_client
 from fedjax.core import optimizers
 from fedjax.core import tree_util
+from fedjax.core import models
 from fedjax.core.typing import BatchExample
 from fedjax.core.typing import Params
 from fedjax.core.typing import PRNGKey
 
 import jax
+import jax.numpy as jnp
 
 Grads = Params
+
+def convex_combination(x_global: Params, x_local: Params, alpha: float) -> Params:
+  """Computes alpha * x_global + (1 - alpha) * x_local for PyTrees."""
+  return tree_util.tree_add(tree_util.tree_weight(x_global, alpha), tree_util.tree_weight(x_local, 1 - alpha))
 
 def create_train_for_each_client(grad_fn):
   """Builds client_init, client_step, client_final for for_each_client."""
   
-  def convex_combination(x_global: Params, x_local: Params, alpha: float) -> Params:
-    """Computes alpha * x_global + (1 - alpha) * x_local for PyTrees."""
-    return tree_util.tree_add(tree_util.tree_weight(x_global, alpha), tree_util.tree_weight(x_local, 1 - alpha))
-
+  print('Debug point 3')
   def client_init(server_params, client_input):
+    print('Debug point 4')
     client_alpha, client_plm, client_rng = client_input
     client_step_state = {
         'params': server_params,
@@ -93,12 +97,18 @@ def fedmix(
       clients: Sequence[Tuple[federated_data.ClientId,
                               client_datasets.ClientDataset, PRNGKey]]
   ) -> Tuple[ServerState, Mapping[federated_data.ClientId, Any]]:
-    batch_clients = [(cid, cds.shuffle_repeat_batch(client_batch_hparams), (alphas[cid], plms[cid], crng))
+    batch_clients = [(cid, cds.shuffle_repeat_batch(client_batch_hparams), [alphas[cid], plms[cid], crng])
                      for cid, cds, crng in clients]
     num_clients = len(clients)
     client_diagnostics = {}
     full_grad = tree_util.tree_zeros_like(server_state.params)
+    print('Debug point 1')
+    print('server_state.params type=',type(server_state.params))
+    print(batch_clients[0])
+    print(type(batch_clients[0][1]))
+    print(type(batch_clients[0][2][1])    
     for client_id, grad in train_for_each_client(server_state.params, batch_clients):
+      print('Debug point 2')
       full_grad = tree_util.tree_add(full_grad, grad)
       # We record the l2 norm of client updates as an example, but it is not
       # required for the algorithm.
@@ -116,3 +126,22 @@ def fedmix(
     return ServerState(params, opt_state)
 
   return federated_algorithm.FederatedAlgorithm(init, apply)
+
+def evaluate_model(model: models.Model, params: Params, 
+                   client_data: Iterable[Tuple[float, Params, client_datasets.ClientDataset]], client_batch_parameters: client_datasets.BatchHParams) -> Dict[str, jnp.ndarray]:
+    """Evaluates FedMix model for multiple batches and returns final results.
+    This is the recommended way to compute evaluation metrics for a given model.
+    Args:
+        model: Model container.
+        params: Pytree of model parameters to be evaluated.
+        client_data: Tuple of client's alpha, pure local model and dataset.
+        client_batch_parameters: Hyperparameters for batching client dataset for evaluation.
+    Returns:
+        A dictionary of evaluation :class:`~fedjax.metrics.Metric` results.
+    """
+    stat = {k: metric.zero() for k, metric in model.eval_metrics.items()}
+    for alpha, plm, cds in client_data:
+      personalized_params = convex_combination(params, plm, alpha)
+      for batch in cds.batch(client_batch_parameters):
+        stat = models._evaluate_model_step(model, personalized_params, batch, stat)
+    return jax.tree_util.tree_map(lambda x: x.result(), stat, is_leaf=lambda v: isinstance(v, metrics.Stat))
